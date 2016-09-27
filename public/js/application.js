053cc7a3cefd245d3181dbe34a771583deba94f3
Sam Parker <samuel.gage.parker@gmail.com>
2016-09-26 23:43:49 -0700

    styling and ajax functions for main menu page

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  
+  $(".hotspot").click(function(event){
+    event.preventDefault();
+    var hsAction = $(this).parent().attr("href");
+    var request = $.ajax({
+      url: hsAction,
+    });
+
+    request.done(function(hotspot){
+      $(".hotspot-menu").hide();
+      $(".hotspot-instructions").hide();
+      // $(".search-bar").hide();
+      // $("#bike-pic").css("bottom", "55%")
+      hsMenu = $(hotspot).find(".hotspot-menu");
+      // $("#solutions-menu").remove(hsMenu);
+      $("#solutions-menu").append(hsMenu);
+    })
+  });
 });
